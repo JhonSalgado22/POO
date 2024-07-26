81465d2de71768052a17f443f6805d5d268dea05
JhonSalgado22 <cuh51239696@cuh.mx>
2024-07-25 22:58:54 -0600

    Create Panadero.java

---
+++ Panadero.java
@@ -0,0 +1,48 @@
+package cuh.com.mx;
+public class Panadero implements Trabajador {
+    private String nombre;
+    private short altura;
+    private short edad;
+    private double sueldo;
+    private boolean trabajando;
+
+    public Panadero(String nombre, short altura, short edad, double sueldo) {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.edad = edad;
+        this.sueldo = sueldo;
+        this.trabajando = false;
+    }
+
+    public String getNombre() {
+        return "Sr." + nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String trabajar() {
+        trabajando = !trabajando;
+        return "Trabajando haciendo pan";
+    }
+
+    @Override
+    public void cobrar() {
+        System.out.println("Pagar sueldo del panadero");
+    }
+
+    @Override
+    public void alimentarse() {
+        System.out.println("Hora de comer del panadero");
+    }
+
+    @Override
+    public void checar() {
+        if (!trabajando)
+            System.out.println("Hora de entrada del panadero registrada");
+        else
+            System.out.println("Hora de salida del panadero registrada");
+    }
+}
